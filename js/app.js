58669f9c60c3af21708d38c6754b645db3b23479
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-07-03 19:10:46 +0530

    night mode button working.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 import { paragraph } from "../db/db.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const nightModeToggle = document.getElementById("night-mode");
+  const lightModeToggle = document.getElementById("light-mode");
+  const themeStylesheet = document.getElementById("theme-stylesheet");
+
+  if (localStorage.getItem("theme") === "light") {
+    themeStylesheet.href = "./style/dark.css";
+  } else {
+    themeStylesheet.href = "./style/light.css";
+  }
+
+  nightModeToggle.addEventListener("click", () => {
+    themeStylesheet.href = "./style/dark.css";
+    localStorage.setItem("theme", "dark");
+  });
+
+  lightModeToggle.addEventListener("click", () => {
+    themeStylesheet.href = "./style/light.css";
+    localStorage.setItem("theme", "light");
+  });
+});
+
